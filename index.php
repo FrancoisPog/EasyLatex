3fd41bc04743f2d50983374caa4d67cdfe18838d
François Poguet <francoispoguet@gmail.com>
2020-05-29 15:36:56 +0200

    Organize the main components of the page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>EasyLatex</title>
+        <link rel="stylesheet" href="styles/easylatex.css">
+    </head>
+    <body id="index">
+        <header>
+            <h1>EasyLatex</h1>
+            <h2>Make Latex document without code !</h2>
+        </header>
+
+        <main>
+            <div class="editor">
+                <div class="editor-input" contenteditable="true">
+                    Markdown content here
+                </div>
+            </div>
+            <div class="viewer">
+                <div class="viewer-wrapper">
+                    <embed  src="https://latexonline.cc/compile?url=https://francois.poguet.com/manual.tex" type="application/pdf">
+                </div>
+            </div>    
+        </main>
+        
+        <footer>
+            <p>EasyLatex - François Poguet &copy;</p>
+        </footer>
+    </body>
+</html>
