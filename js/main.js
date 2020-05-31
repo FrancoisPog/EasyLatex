85203ef88ba40d4896b9fa7786ec744310ab1fd1
François Poguet <francoispoguet@gmail.com>
2020-05-31 12:02:38 +0200

    text transformation seems to work

--- js/main.js
+++ js/main.js
@@ -4,42 +4,44 @@ let btn_convert = document.getElementById('btn-convert');
 
 let form = document.querySelector('form');
 
-const regex_m2p = { 
-                    '<em>$1</em>' : /\[i\]([^\[]*((\[[^i])?([^[]*?))*)_/gim,
-                        
-                                             
-                }
+const markdown_regex    = /\[(.):(.*?):(\1)\]/gmi;
+const preview_regex     = /<(.*?)>(.*?)<\/(\1)>/gmi;
 
-const regex_p2m = { 
-                    '[i]$1_' : /<em>(.*?)<\/em>/gim,
-                        
-                }
+function parseMarkdown(match,p1,p2,p3,offset,og){
+    console.log(match,"\t- ",p1,"\t- ",p2,"\t- ",p3,"\t- ",offset,"\t- ",og);
 
+    if(p2.match(markdown_regex)){
+        p2 = p2.replace(markdown_regex,parseMarkdown);
+    }
 
-const regex_p2l = {
-                        '\\section{$1}' : /<h3> *(.*?)<\/h3>/gmi,
-                        '\\subsection{$1}' : /<h4>(.*?)<\/h4>/gmi,
-                        '\\textit{$1}' : /<em>(.*?)<\/em>/gmi,
-                        '\\textbf{$1}' : /<strong>(.*?)<\/strong>/gmi,
-                        ' ' : /<br ?\/?>/gmi
-                    }
+    if(p1 == 'i'){
+        return '<em>'+p2+'</em>';
+    }
 
+    if(p1 == 'b'){
+        return '<strong>'+p2+'</strong>';
+    }
 
-function parseMarkdown(match,p1,p2,offset,og){
-    console.log(match,p1,p2,offset,og);
-
-    for (const regex_type in regex_m2p) {
-        p1 = p1.replace(regex_m2p[regex_type],parseMarkdown);
-    } 
+    console.log("error");
+   
+    
 }
 
 function markdown2preview(){
     let editor = document.getElementsByClassName("editor-input")[0];
     let content = editor.value;
     
-    for (const regex_type in regex_m2p) {
-        content = content.replace(regex_m2p[regex_type],parseMarkdown);
-    } 
+    if(content.match(/\[(.):([^\[\]])*(\[\1)/gmi)){
+        alert("Syntax error");
+        return;
+    }
+
+    content = content.replace(/\n/gmi,'<br>');
+    
+    content = content.replace(markdown_regex,parseMarkdown);
+
+    console.log(content);
+    
 
     let preview = document.createElement("div");
     preview.classList.add('editor-input');
@@ -48,16 +50,41 @@ function markdown2preview(){
     editor.parentNode.replaceChild(preview,editor);
 
     editor.innerHTML = content;
+    preview_mode = true;
 }
+
+function parsePreview(match,p1,p2,p3,offset,og){
+    console.log(match,"\t- ",p1,"\t- ",p2,"\t- ",p3,"\t- ",offset,"\t- ",og);
+
+    if(p2.match(preview_regex)){
+        p2 = p2.replace(preview_regex,parsePreview);
+    }
+
+    if(p1 == 'em'){
+        return '[i:'+p2+':i]';
+    }
+
+    if(p1 == 'strong'){
+        return '[b:'+p2+':b]';
+    }
+
+    console.log("error"+p1);
+   
+    
+}
+
 
 function preview2markdown(){
     let preview = document.getElementsByClassName("editor-input")[0];
     let content = preview.innerHTML;
 
-    for (const regex_type in regex_p2m) {
-        content = content.replace(regex_p2m[regex_type],regex_type);
-        
-    }
+    
+
+    content = content.replace(preview_regex,parsePreview);
+
+    content = content.replace(/<br>/gmi,'\n');
+
+    console.log(content);
 
     let area = document.createElement("textarea");
     area.classList.add('editor-input');
@@ -65,6 +92,7 @@ function preview2markdown(){
     area.name = "content";
 
     preview.parentNode.replaceChild(area,preview);
+    preview_mode = false;
 }
 
 
@@ -92,10 +120,10 @@ btn_preview.addEventListener('click',function(e){
     e.preventDefault();
     if(preview_mode){
         preview2markdown();
-        preview_mode = false;
+        
     }else{
         markdown2preview();  
-        preview_mode = true;
+        
     }
 });
 
